4bc468034056e6fb9292e5ca93cd57b580ce7134
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-06-12 15:39:55 -0400

    feat: create Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import { Header } from './components/Header';
+import styles from './App.module.scss';
+
 import './styles/global.scss';
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 
 function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo obcaecati sunt quia aperiam alias accusantium dicta totam culpa a odio, temporibus nihil itaque ipsam labore quos eveniet iure reprehenderit unde!
+        </main>
+      </div>
+    </div>
   )
 };
 
